e063fdc43fe06d28b9f157cf50acfa4414e42d39
My name <natalia.udaltsova@gmail.com>
2023-04-20 21:45:33 +0100

    tmp commit rollback

---
+++ src/main/webapp/environments/environment.dev.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  apiBaseUrl: 'http://localhost:8080/api',
+  itemsPerPage: 10,
+  production: false,
+  auth_required : true,
+  ad_auth_scopes: ['Default.Scope'],
+  ad_api_scopes:  ['api://c9aa0b41-281f-43ff-b960-4ef9c2edbba2/Default.Scope'],
+  ad_client_id: "29c569e7-6911-498c-ab86-4272079319fa",
+  ad_tenant_id: "96ca42b9-e07d-40dd-a3e0-e0fb3847415f",
+  ad_redirect_uri: "http://localhost:4200"
+};
